070ff6fbc8dad8a15b9646ef5142cc19623d73d9
Robbie R <git@robbie.computer>
2022-05-12 12:58:46 +0100

    fish: don't use pyenv by default

--- tag-fish/config/fish/config.fish
+++ tag-fish/config/fish/config.fish
@@ -52,15 +52,18 @@ alias bigfolders "du -k ~ | awk '$argv > 50000' | sort -nr"
 alias cwd "pwd | tr -d '\n' | pbcopy"
 alias path 'echo $PATH | tr -s ":" "\n"'
 alias macsleep "pmset displaysleepnow"
+alias resetwifi 'networksetup -setairportpower en0 off; networksetup -setairportpower en0 on'
 
 # venv
 abbr avenv 'source venv/bin/activate'
 abbr venv 'virtualenv venv -p $argv[1]'
 
 # pyenv
-source (pyenv init - | psub)
-status --is-interactive; and pyenv init - | source
-status --is-interactive; and pyenv virtualenv-init - | source
+# source (pyenv init - | psub)
+# status --is-interactive; and pyenv init - | source
+# status --is-interactive; and pyenv virtualenv-init - | source
+# status is-login; and pyenv init --path | source
+# pyenv init - | source
 
 # useful commands at work
 abbr ytuw 'yarn test:unit:watch'

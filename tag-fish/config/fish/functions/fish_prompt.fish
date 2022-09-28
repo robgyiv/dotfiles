43cd86a6e9233804c56cc8c5ba1c76b094be84b9
Robbie R <robbie@faculty.ai>
2022-09-28 20:55:04 +0100

    fish: change prompt, use homedir for nvm

--- tag-fish/config/fish/functions/fish_prompt.fish
+++ tag-fish/config/fish/functions/fish_prompt.fish
@@ -4,12 +4,14 @@ set yellow (set_color yellow)
 set green (set_color green)
 set red (set_color red)
 set foobar (set_color 333333)
+# black, red, green, yellow, blue, magenta, cyan, white
 
 # Fish git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch blue
+# set __fish_git_prompt_color_branch 118ab2
+set __fish_git_prompt_color_branch normal
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
@@ -25,7 +27,12 @@ set __fish_git_prompt_char_upstream_prefix ""
 function fish_prompt
   set last_status $status
 
-  set_color purple
+  # if set -q VIRTUAL_ENV
+  #   echo -n -s (set_color yellow) "[" (basename "$VIRTUAL_ENV") "]" (set_color normal) " "
+  # end
+
+  # set_color 06d6a0
+  set_color magenta
   printf '%s' (prompt_pwd)
   set_color normal
 
--- tag-fish/config/fish/functions/nvm.fish
+++ tag-fish/config/fish/functions/nvm.fish
@@ -1,5 +1,5 @@
 function nvm
   set -x NVM_DIR "$HOME/.nvm"
-  bass source /usr/local/opt/nvm/nvm.sh --no-use ';' nvm $argv
+  bass source ~/.nvm/nvm.sh --no-use ';' nvm $argv
 end
 

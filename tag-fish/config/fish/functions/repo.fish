1df6ac5aa319b9e16cd8a5f5c6477fe567ef939b
Robbie R <robbie@faculty.ai>
2023-03-27 10:00:31 +0100

    fish: start tmux session from repo function

--- tag-fish/config/fish/functions/repo.fish
+++ tag-fish/config/fish/functions/repo.fish
@@ -2,6 +2,8 @@
 function repo
     set initial_query $argv
     set code_dir "/Users/robbie/code"
-    set dest (git-find-rs "$code_dir" | fzy -q "$initial_query" -l 20) && cd "$dest"
-    tmux new -s (basename (pwd))
+    set dest (git-find-rs "$code_dir" | fzy -q "$initial_query" -l 10) && cd "$dest"
+    if test $dest
+        tmux new -s (basename (pwd))
+    end
 end

46a7410a44d0323714825a63f7570386e292d195
Robbie R <robbie@faculty.ai>
2023-06-16 11:18:09 +0100

    fish: git-find-rs attach to existing tmux session if present

--- tag-fish/config/fish/functions/repo.fish
+++ tag-fish/config/fish/functions/repo.fish
@@ -1,9 +1,9 @@
 # https://github.com/robgyiv/git-find-rs
 function repo
     set initial_query $argv
-    set code_dir "/Users/robbie/code"
+    set code_dir /Users/robbie/code
     set dest (git-find-rs "$code_dir" | fzy -q "$initial_query" -l 10) && cd "$dest"
     if test $dest
-        tmux new -s (basename (pwd))
+        tmux new -A -s (basename (pwd))
     end
 end

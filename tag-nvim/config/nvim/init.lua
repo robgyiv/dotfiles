ba9b399f5509e0c97edeab0250ad0cc6ee37c566
Robbie R <git@robbie.computer>
2024-03-30 11:04:54 +0000

    nvim: Develop `git-web-line.nvim` locally

--- tag-nvim/config/nvim/init.lua
+++ tag-nvim/config/nvim/init.lua
@@ -1,6 +1,8 @@
-require('plugins')     -- lua/plugins.lua
-require('settings')    -- lua/settings.lua
-require('maps')        -- lua/maps.lua
-require('lsp')         -- lua/lsp.lua
-require('treesitter')  -- lua/treesitter.lua
+-- local development
+vim.opt.runtimepath:prepend("$HOME/code/github.com/robgyiv/git-web-line.nvim")
 
+require("plugins") -- lua/plugins.lua
+require("settings") -- lua/settings.lua
+require("maps") -- lua/maps.lua
+require("lsp") -- lua/lsp.lua
+require("treesitter") -- lua/treesitter.lua

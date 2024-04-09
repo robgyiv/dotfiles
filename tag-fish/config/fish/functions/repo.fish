e5ac5c000d397c27b8006a19dfa5e1416cabd9a7
Robbie R <git@robbie.computer>
2024-04-09 09:08:28 +0100

    fish: update `git-find-rs` CLI

--- tag-fish/config/fish/functions/repo.fish
+++ tag-fish/config/fish/functions/repo.fish
@@ -2,7 +2,7 @@
 function repo
     set initial_query $argv
     set code_dir $HOME/code
-    set dest (git-find-rs "$code_dir" | fzy -q "$initial_query" -l 10) && cd "$dest"
+    set dest (git-find-rs -d "$code_dir" -m 5 | fzy -q "$initial_query" -l 10) && cd "$dest"
     if test $dest
         tmux new -A -s (basename (pwd))
     end

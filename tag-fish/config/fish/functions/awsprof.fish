ec3fc19c1faf41359812f55ba12e1dec8bf2db70
Robbie R <robbie@faculty.ai>
2024-06-19 14:25:36 +0100

    fish: fix awsprof implementation

--- tag-fish/config/fish/functions/awsprof.fish
+++ tag-fish/config/fish/functions/awsprof.fish
@@ -1,33 +1,21 @@
 # Sets up the AWS profile environment variables using fzf and the profile in ~/.aws/config
 function awsprof
   argparse 'l/login' 'k/kubeconfig' -- $argv
-  set -l login_flag 0
-  set -l kubeconfig_flag 0
 
-  # Handle options
-  for name in (set -e argv; string split ' ' -- $argv)
-    switch $name
-        case '-l' '--login'
-            set login_flag 1
-        case '-k' '--kubeconfig'
-            set kubeconfig_flag 1
-    end
-  end
-
-  if test -f ~/.aws/config
-    set profile (cat ~/.aws/config | grep '\[profile ' | awk '{print $2}' | sed 's/\]//' | fzf) # Needs to remove the `]` character
+  if test -f ~/.aws/config && test -n "$_flag_login"
+    set profile (cat ~/.aws/config | grep '\[profile ' | awk '{print $2}' | sed 's/\]//' | fzf) # Removes the `]` character
     export AWS_PROFILE="$profile"
     echo "AWS_PROFILE set to $profile"
-  end
-  if test $login_flag -eq 1
     aws-sso-util login
   end
 
-  if test $kubeconfig_flag -eq 1
-    # Use `fd` to find eks-*.yaml with wildcard files and set $KUBECONFIG to its absolute path
-    set kubeconfig (fd eks -e yaml | fzf)
-    export KUBECONFIG="$kubeconfig"
-    echo "KUBECONFIG set to $kubeconfig"
+  if test -n "$_flag_kubeconfig"
+    set fd_files (fd eks -e yaml | string collect -N)
+    if test -n "$fd_files"
+      set kubeconfig (echo "$fd_files" | fzf)
+      export KUBECONFIG="$kubeconfig"
+      echo "KUBECONFIG set to $kubeconfig"
+    end
   end
 end
 

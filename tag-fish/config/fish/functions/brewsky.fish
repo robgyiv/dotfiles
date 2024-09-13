1c41d6d0bb6a60aa5429987a945df71064660e3f
Robbie R <git@robbie.computer>
2024-09-13 17:45:47 +0100

    fish: `brewsky` error handling

--- tag-fish/config/fish/functions/brewsky.fish
+++ tag-fish/config/fish/functions/brewsky.fish
@@ -1,9 +1,15 @@
 function brewsky
   set -l package $argv[1]
   brew info $package
+  if test $status -eq 1
+    return 1
+  end
   read -l -P 'Install this package? [y/N]'\n confirm
   switch $confirm
     case 'y'
       brew install $package
+    case '*'
+      echo $package not installed
+      return 1
   end
 end

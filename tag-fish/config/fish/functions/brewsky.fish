b55ccd0a322f9282353888c431b79d93f3319f5b
Robbie R <git@robbie.computer>
2024-09-13 16:08:55 +0100

    fish: `brewsky` function to search, confirm package

---
+++ tag-fish/config/fish/functions/brewsky.fish
@@ -0,0 +1,9 @@
+function brewsky
+  set -l package $argv[1]
+  brew info $package
+  read -l -P 'Install this package? [y/N]'\n confirm
+  switch $confirm:
+    case 'y':
+      brew install $package
+    end
+end

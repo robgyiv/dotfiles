f128dac1eb7ff51370fdf216857b5213845b8642
Robbie R <git@robbie.computer>
2024-09-13 16:13:38 +0100

    fish: fix syntax

--- tag-fish/config/fish/functions/brewsky.fish
+++ tag-fish/config/fish/functions/brewsky.fish
@@ -2,8 +2,8 @@ function brewsky
   set -l package $argv[1]
   brew info $package
   read -l -P 'Install this package? [y/N]'\n confirm
-  switch $confirm:
-    case 'y':
+  switch $confirm
+    case 'y'
       brew install $package
-    end
+  end
 end

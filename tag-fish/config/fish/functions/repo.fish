90f28e2a99019482dac65782dd95f0d18d438a15
Robbie R <git@robbie.computer>
2025-10-28 07:06:14 +0000

    fish: fix `repo` to change tmux session, not cd to repo dir

--- tag-fish/config/fish/functions/repo.fish
+++ tag-fish/config/fish/functions/repo.fish
@@ -2,14 +2,18 @@
 function repo
     set initial_query $argv
     set code_dir $HOME/code
-    set dest (git-find-rs -d "$code_dir" -m 5 | fzy -q "$initial_query" -l 10) && cd "$dest"
-    if test $dest
-        set session_name (basename (pwd) | string replace -a -r '[.:\s]' '_')
+    set dest (git-find-rs -d "$code_dir" -m 5 | fzy -q "$initial_query" -l 10)
+
+    if test -n "$dest"
+        set session_name (basename "$dest" | string replace -a -r '[.:\s]' '_')
+
         if test -n "$TMUX"
-            tmux new -d -s "$session_name"
+            # Create new detached session starting in $dest, then switch to it
+            tmux new-session -d -s "$session_name" -c "$dest"
             tmux switch-client -t "$session_name"
         else
-            tmux new -A -s "$session_name"
+            # Outside tmux: attach or create, starting in $dest
+            tmux new-session -A -s "$session_name" -c "$dest"
         end
     end
 end
